2e420795a8e1a1749ad4d55c5d56a957368bc6c3
sudoskys <coldlando@hotmail.com>
2024-09-29 11:20:18 +0800

    :bug: fix: change low_memory flag to True in detect_multilingual test

--- feature_test/__init__.py
+++ feature_test/__init__.py
@@ -6,7 +6,7 @@
 from fast_langdetect import detect, detect_multilingual, detect_language
 
 # 测试繁体，简体，日文，英文，韩文，法文，德文，西班牙文
-print(detect_multilingual("Hello, world!你好世界!Привет, мир!",low_memory=False))
+print(detect_multilingual("Hello, world!你好世界!Привет, мир!", low_memory=True))
 print(detect_multilingual("Hello, world!你好世界!Привет, мир!"))
 # [{'lang': 'ja', 'score': 0.32009604573249817}, {'lang': 'uk', 'score': 0.27781224250793457}, {'lang': 'zh', 'score': 0.17542070150375366}, {'lang': 'sr', 'score': 0.08751443773508072}, {'lang': 'bg', 'score': 0.05222449079155922}]
 print(detect("hello world"))
