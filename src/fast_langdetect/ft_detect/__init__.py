ccacd483c7b3e9304f24dfb4d30877700c54b0a7
sudoskys <coldlando@hotmail.com>
2025-01-11 19:58:06 +0800

    ✨ refactor(app): remove deprecated detect_langs function
    The `detect_langs` function was removed to streamline the codebase.
    This change eliminates redundant functionality and encourages the use
    of the updated `detect_language` method, enhancing maintainability.

--- src/fast_langdetect/ft_detect/__init__.py
+++ src/fast_langdetect/ft_detect/__init__.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2024/1/17 下午4:00
-import logging
 
 from .infer import detect
 from .infer import detect_multilingual  # noqa: F401
@@ -24,14 +23,3 @@ def detect_language(sentence, *, low_memory: bool = True):
     if lang_code == "JA" and not is_japanese(sentence):
         lang_code = "ZH"
     return lang_code
-
-
-def detect_langs(sentence, *, low_memory: bool = True):
-    """
-    Detect language
-    :param sentence: str sentence
-    :param low_memory: bool (default: True) whether to use low memory mode
-    :return: ZH, EN, JA, KO, FR, DE, ES, .... (two uppercase letters)
-    """
-    logging.warning("detect_langs is deprecated, use detect_language instead")
-    return detect_language(sentence, low_memory=low_memory)
